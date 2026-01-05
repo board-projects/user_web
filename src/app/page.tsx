8c47e0719494977f7cf976bfce941dd1dc74409e
mahankabir <mahan.kabir@gmail.com>
2026-01-05 03:13:18 +0200

    feat(app): add metadata seo.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,36 @@
 import Link from "next/link";
 
+export const metadata = {
+    title: "Real-time Collaborative Board - MahanKabir",
+    description: "Draw, collaborate, and share your designs in real-time with your team. Invite anyone with a link and start designing together!",
+    keywords: "real-time, collaborative board, online design, team design, web socket, MahanKabir",
+    authors: [{ name: "MahanKabir", url: "https://www.mahankabir.ir" }],
+    openGraph: {
+        title: "Real-time Collaborative Board",
+        description: "Draw and collaborate in real-time. Share your board with anyone via a link!",
+        url: `${process.env.NEXT_PUBLIC_FRONTEND_URL}`,
+        siteName: "Dr. Monk",
+        images: [
+            {
+                url: `${process.env.NEXT_PUBLIC_FRONTEND_URL}/og-image.png`,
+                width: 1200,
+                height: 630,
+                alt: "Collaborative Board Screenshot",
+            },
+        ],
+        locale: "en_US",
+        type: "website",
+    },
+    twitter: {
+        card: "summary_large_image",
+        title: "Real-time Collaborative Board",
+        description: "Draw and collaborate in real-time with your team. Invite anyone with a link!",
+        images: [`${process.env.NEXT_PUBLIC_FRONTEND_URL}/og-image.png`],
+    },
+};
+
 export default function Home() {
+
     return (
         <main className="relative flex h-screen justify-center items-center bg-cyan-50 px-4">
             <Link

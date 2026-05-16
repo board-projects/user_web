85801288d083182d52886e89cb5fc97fffc9c36a
mahankabir <mahan.kabir@gmail.com>
2026-05-16 21:50:53 +0300

    fix(auth): change auth cookie to client cookie.

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://back.dequizma.com/api/:path*',
+      },
+    ];
+  },
   reactStrictMode: true,
 };
 
